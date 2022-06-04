62f938e292c150d6d378f1b667b7cffaa75f527f
Gabriela Patricia Rodriguez <86595302+GabrielaPatriciaRodriguez@users.noreply.github.com>
2022-06-04 15:41:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./styles.css";
+
+import Navigation from "./components/Navigation";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <h1>Mi tienda Virtual</h1>
+        <p>Gabriela P. Rodriguez</p>
+      </div>
+    );
+  }
+}
+
+export default App;
